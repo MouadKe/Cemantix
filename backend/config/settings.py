6d844ec8e3ba45774bf49953b904b21bf606f013
Imaaann <iman555bpe147@gmail.com>
2026-02-19 18:16:33 +0100

    switching to short pooling arch

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -40,9 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'channels',
     'users',
-    'games'
 ]
 
 REST_FRAMEWORK = {
--- backend/games/routing.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .consumers import EchoConsumer
-
-websocket_urlpatterns = [
-    path("ws/echo/", EchoConsumer.as_asgi())    
-]
